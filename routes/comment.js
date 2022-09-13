de74a01b02fa81370d81d11563dfdb06e63ac259
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2022-09-13 09:12:40 +0800

    update

--- routes/comment.js
+++ routes/comment.js
@@ -2,9 +2,9 @@ const router = require("express").Router();
 const Post = require("../models/Post");
 const User = require("../models/User");
 const Comment = require("../models/Comments");
+const auth = require("../middleware/auth");
 
-
-router.post("/:id/create", async (req, res) => {
+router.post("/:id/create", auth, async (req, res) => {
     const newComment = new Comment(req.body);
     try {
         const saveComment = await newComment.save();
